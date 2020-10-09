61c8965a2f252fb9d563ee2002d487f93085b859
mwdoyle <mwdoyle@sfu.ca>
2020-10-08 23:13:48 -0700

    add test cases

--- test/Dummy.cpp
+++ test/Dummy.cpp
@@ -5,19 +5,6 @@
 using namespace testing;
 using namespace shapes;
 
-TEST(ParallelogramTests, ParallelogramTestZero)
-{
-    Parallelogram parallelogram{Side{5}, Side{5}, Angle{20}};
-    std::cerr << "getArea = " << parallelogram.getArea() << std::endl;
-    std::cerr << "isRectangle = " << parallelogram.isRectangle() << std::endl;
-    std::cerr << "isRhombus = " << parallelogram.isRhombus() << std::endl;
-    std::cerr << "isSquare = " << parallelogram.isSquare() << std::endl;
-    //    std::cerr << "getKind = " << parallelogram.getKind() << std::endl;
-    std::cerr << "getPerimeter = " << parallelogram.getPerimeter() << std::endl;
-
-    EXPECT_TRUE(false);
-}
-
 /*
  * Task 1
  * Create three distinct and independent test cases that result in failure. You should reason about the correct and
@@ -26,32 +13,67 @@ TEST(ParallelogramTests, ParallelogramTestZero)
  * Grading may include runing your tests on corrected implementations to verify the behaviors.
 */
 
-TEST(ParallelogramTests, ParallelogramTestOne)
+// for kind consider angles and sides
+TEST(ParallelogramTests, getPerimeterTest)
 {
-    EXPECT_TRUE(false);
+    Parallelogram parallelogram{Side{50}, Side{60}, Angle{45}};
+
+    // calculate the perimeter
+    // 2*(s1 + s2) = 2(50 + 60) = 220
+
+//    std::cerr << "getPerimeter = " << parallelogram.getPerimeter() << std::endl;
+    // getPerimeter returns 200
+
+    ASSERT_EQ(parallelogram.getPerimeter(), 220);
 }
 
-TEST(ParallelogramTests, ParallelogramTestTwo)
+
+TEST(ParallelogramTests, getAreaTest)
 {
-    EXPECT_TRUE(false);
+    Parallelogram parallelogram{Side{50}, Side{60}, Angle{45}};
+
+    // calculate the area
+    // s1 * s2 * Sin(interior) = A
+    // 50 * 60 * Sin(45) =~ 2121.3203435596425732025330863145471178545078130654221097650196069
+
+//    std::cerr << "getArea = " << parallelogram.getArea() << std::endl;
+    // getArea returns 2545.58
+
+    // comparing doubles can cause issues so I used round
+    ASSERT_EQ(round(parallelogram.getArea()), round(2121.32));
 }
 
-TEST(ParallelogramTests, ParallelogramTestsThree)
+TEST(ParallelogramTests, isRectangleTest)
 {
-    EXPECT_TRUE(false);
+    // getKind
+    Parallelogram parallelogramSquare{Side{50}, Side{50}, Angle{89.99999999999999999}};
+    Parallelogram parallelogramRectangle{Side{50}, Side{60}, Angle{89.99999999999999999}};
+
+    // test isRectangle
+    // 0 is not rect
+    // 1 is rectangle
+//    std::cerr << "isRectangle = " << parallelogramSquare.getKind() << std::endl;
+//    std::cerr << "isRectangle = " << parallelogramSquare.isRectangle() << std::endl;
+//    std::cerr << "isRectangle = " << parallelogramRectangle.getKind() << std::endl;
+//    std::cerr << "isRectangle = " << parallelogramRectangle.isRectangle() << std::endl;
+
+    // every square is a rectangle but every rectangle is not a square
+    EXPECT_EQ(false, parallelogramRectangle.isRectangle());
+    EXPECT_EQ(false, parallelogramSquare.isRectangle());
 }
 
-TEST(ParallelogramTests, ParallelogramTestsFour)
-{
-    EXPECT_TRUE(false);
-}
 
-TEST(ParallelogramTests, ParallelogramTestsFive)
-{
-    EXPECT_TRUE(false);
-}
-
-TEST(ParallelogramTests, ParallelogramTestsSix)
-{
-    EXPECT_TRUE(false);
-}
+//TEST(ParallelogramTests, ParallelogramTestsFour)
+//{
+//    EXPECT_TRUE(false);
+//}
+//
+//TEST(ParallelogramTests, ParallelogramTestsFive)
+//{
+//    EXPECT_TRUE(false);
+//}
+//
+//TEST(ParallelogramTests, ParallelogramTestsSix)
+//{
+//    EXPECT_TRUE(false);
+//}
